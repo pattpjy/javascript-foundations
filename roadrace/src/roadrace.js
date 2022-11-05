dd894638aa911b6c9a2a90029b6a6ec4530e7782
Patt Sookmark <pijiya@sookmark.com>
2022-11-05 10:46:46 -0600

    Add function to roadrace'

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,4 +1,27 @@
+const { Runner } = require("mocha")
+
 class Roadrace {
+    constructor(obj){
+        this.name = obj.title
+        this.location = obj.city
+        this.distance = 0
+        this.participants = []
+    }
+    setDistance(dist){
+        this.distance = dist
+        return `The ${this.name} in ${this.location} is a ${dist} mile race.`
+    }
+    registerParticipants(runnerObj){
+        this.participants.push(runnerObj)
+    }
+    completeRace(){
+        for (var i = 0; i < this.participants.length; i++){
+            this.participants[i].completedRaces.push(this.name)
+            this.participants[i].milesRun += this.distance
+        }
+       
+        
+    }
 
 }
 
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,24 @@
 class Runner {
-
+    constructor(name,age){
+        this.name = name
+        this.age = age
+        this.milesRun = 0
+        this.fitness = 25
+        this.completedRaces = []
+    }
+    runSomeMiles(miles){
+    this.milesRun += miles
+    this.fitness += miles
+    }
+    stretch(){
+        this.fitness += 0.5
+    }
+    runRace(lo,miles){
+        this.completedRaces.push(lo)
+        this.milesRun += miles
+        this.fitness += miles
+    }
+    
 }
 
 module.exports = Runner;
